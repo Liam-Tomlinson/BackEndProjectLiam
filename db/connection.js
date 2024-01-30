b4be00b46f893ca30ee3826a256eeb28f96b2161
Liam Tomlinson <liamtomlinson@Liams-Air.northcoders.com>
2024-01-30 10:33:21 +0000

    change file directory

--- db/connection.js
+++ db/connection.js
@@ -1,19 +1,54 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
-
-const config = {}
-
-if (ENV === 'production') {
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
+require("dotenv").config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
+const config = {};
+if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
+module.exports = new Pool(config);
 
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
 
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE not set');
-}
 
-module.exports = new Pool();
+
+
+// const { Pool } = require('pg');
+// const ENV = process.env.NODE_ENV || 'development';
+
+// const config = {}
+
+// if (ENV === 'production') {
+//   config.connectionString = process.env.DATABASE_URL;
+//   config.max = 2;
+// }
+
+// require('dotenv').config({
+//   path: `${__dirname}/../.env.${ENV}`,
+// });
+
+// if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+//   throw new Error('PGDATABASE not set');
+// }
+
+// module.exports = new Pool(config);›
+
+
+
+
+// const { Pool } = require('pg');
+
+// const ENV = process.env.NODE_ENV || 'development';
+// const config = {};
+
+// if (ENV === 'production') {
+//   config.connectionString = process.env.DATABASE_URL;
+//   config.max = 2;
+// }
+
+// module.exports = new Pool(config);
+
